b4e9599d118ba0abaa71aea6cae3b2e980fc4f45
Marlon Monroy <marlon@monroy.io>
2020-12-05 15:00:18 -0800

    working with html files and routing using get and url

--- clase-2/app.js
+++ clase-2/app.js
@@ -1,8 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "application/json" })
-  res.write('')
+  res.write('hola');
   res.end();
 });
 
