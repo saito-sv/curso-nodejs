82df68de34e5308cd1365511ca590418fd071083
Marlon Monroy <marlon@monroy.io>
2020-12-16 19:54:32 -0800

    clase 8

--- models/post.js
+++ models/post.js
@@ -1,16 +1,9 @@
-export const posts = [{
-  title: "Post 1",
-  time: "Dec 09 2020",
-  body: "Elit eiusmod veniam magna laboris est aliqua. Laborum aliquip pariatur laboris tempor incididunt sunt Lorem aliquip quis velit amet. Veniam ipsum cillum laboris velit exercitation ipsum quis consequat proident ullamco deserunt qui. Ex ipsum ea est exercitation. Id labore esse amet dolor ad Lorem. Consequat irure officia est adipisicing magna est duis incididunt."
-},
-{
-  title: "Post 2",
-  time: "Dec 09 2020",
-  body: "Elit eiusmod veniam magna laboris est aliqua. Laborum aliquip pariatur laboris tempor incididunt sunt Lorem aliquip quis velit amet. Veniam ipsum cillum laboris velit exercitation ipsum quis consequat proident ullamco deserunt qui. Ex ipsum ea est exercitation. Id labore esse amet dolor ad Lorem. Consequat irure officia est adipisicing magna est duis incididunt."
-},
-{
-  title: "Post 3",
-  time: "Dec 09 2020",
-  body: "Elit eiusmod veniam magna laboris est aliqua. Laborum aliquip pariatur laboris tempor incididunt sunt Lorem aliquip quis velit amet. Veniam ipsum cillum laboris velit exercitation ipsum quis consequat proident ullamco deserunt qui. Ex ipsum ea est exercitation. Id labore esse amet dolor ad Lorem. Consequat irure officia est adipisicing magna est duis incididunt."
-}
-]
+import mongoose from 'mongoose'
+const { Schema } = mongoose
+const postSchema = new Schema({
+  title: String,
+  body: String
+});
+
+export const Post = mongoose.model('Post', postSchema)
+
